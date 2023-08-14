f37562acf739f7e5df7b7caa7cb40329f6d83b22
InsaneTurtleX <stalker98cs@gmail.com>
2023-08-14 19:35:04 +0300

    Add Postgres

---
+++ events/модули.py
@@ -0,0 +1,20 @@
+from random import randint as ri
+from random import shuffle as sf
+
+
+b = []
+for i in range(10):
+    a = ri(1, 10)
+    b.append(a)
+
+
+
+print(b)
+sf(b)
+print(b)
+sf(b)
+print(b)
+sf(b)
+print(b)
+
+
